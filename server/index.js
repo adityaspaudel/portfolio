4a60fdff2793c6250a87651c29ba3fe1e014bf38
Aaditya Paudel <adityaspaudel@gmail.com>
2026-01-13 19:24:39 +0545

    Remove node_modules and env files from repo

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT; // Updated port to match your example
-
+const MONGODB_URI =
+	process.env.MONGODB_URI ||
+	"mongodb+srv://portfolio:portfolio@cluster0.tkw7ktb.mongodb.net/?appName=Cluster0";
 // MongoDB Connection
 const dbConnect = async () => {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/portfolioDb");
-		console.log("Successfully connected to MongoDB");
+		await mongoose.connect(MONGODB_URI);
+		console.log(`Successfully connected to MongoDB, ${MONGODB_URI}`);
 	} catch (err) {
 		console.error("Error occurred:", err);
 	}
